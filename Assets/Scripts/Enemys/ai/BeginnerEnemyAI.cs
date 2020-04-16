8d5f26acf43575fe2a6369ae7cea785946af7b6a
v.gyrdasov <v.gyrdasov@gamesbannernet.com>
2020-04-16 09:50:08 +0700

    minor enemy ai changes

--- Assets/Scripts/Enemys/ai/BeginnerEnemyAI.cs
+++ Assets/Scripts/Enemys/ai/BeginnerEnemyAI.cs
@@ -7,7 +7,7 @@ using BaseAI;
 public class BeginnerEnemyAI : BaseEnemyAiClass
 {
     ActionNode checkLowHealth;
-    ActionNode checkALive;
+    ActionNode checkAlive;
     ActionNode checkCanMove;
     ActionNode abilityOn;
 
@@ -34,6 +34,33 @@ public class BeginnerEnemyAI : BaseEnemyAiClass
 
     public BeginnerEnemyAI(CharacterModel model) {
 		this.cModel = model;
+	}
+	
+	public BT.States abilityOnAction()
+    {
+		//TODO: abilityOn
+        return cModel.isLowHealth() ? States.SUCCESS : States.FAILED;
+	}
+
+	public BT.States isCanSearchForTarget()
+	{
+		//TODO: isCanSearchForTarget
+		return cModel.isLowHealth() ? States.SUCCESS : States.FAILED;
+	}
+
+	public BT.States checkCanAttackAction() {
+		//TODO: checkCanAttackAction
+		return cModel.isLowHealth() ? States.SUCCESS : States.FAILED;
+	}
+
+	public BT.States isCanWeaponReachTarget() {
+		//TODO: isCanWeaponReachTarget
+		return cModel.isLowHealth() ? States.SUCCESS : States.FAILED;
+	}
+
+	public BT.States AttackAtPointAction() {
+		//TODO: AttackAtPointAction
+		return cModel.isLowHealth() ? States.SUCCESS : States.FAILED;
 	}
 
 
@@ -68,7 +95,32 @@ public class BeginnerEnemyAI : BaseEnemyAiClass
     {
         checkLowHealth = new ActionNode(isLowHealth, "checkLowHealth");
         checkUnderAttack = new ActionNode(isUnderAttack, "checkUnderAttack");
+		checkCanMove = new ActionNode(canMoveAction, "checkCanMove");
+		abilityOn = new ActionNode(abilityOnAction, "abilityOn");
+		canSearchForTarget = new ActionNode (isCanSearchForTarget, "isCanSearchForTarget");
+    //search for player
+		searchForTargetSequence = new SequenceNode (new List<Node> {
+			//TODO: check it's don't busy by any another sequence
+			checkUnderAttack,
+			canSearchForTarget
+		}, "searchForTargetSequence");
 
+		checkCanAttack = new ActionNode (checkCanAttackAction, "checkCanAttack");
+		canWeaponReachTarget = new ActionNode (isCanWeaponReachTarget, "canWeaponReachTarget");
+		AttackAtPoint = new ActionNode (AttackAtPointAction, "AttackAtPoint");
+		attackTargerSequence = new SequenceNode (new List<Node> {
+			checkCanAttack,
+			canWeaponReachTarget,
+			AttackAtPoint
+		}, "attackTargerSequence");
+
+    //move to target
+    public SequenceNode moveToTarget;
+    //TODO: Inverter node!
+    ActionNode checkNotUnderAttack;
+    //Invert
+    //ActionNode canWeaponReachTarget;
+    ActionNode MoveToPoint;
 
         rootNode = new SelectorNode(new List<Node>
         {
